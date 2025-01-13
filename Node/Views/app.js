6decd85ffeac5a0d8c8b70c57e77b5bf508c283e
heren <heren1heren@gmail.com>
2025-01-13 07:55:59 -0700

    ...

--- Node/Views/app.js
+++ Node/Views/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('node:path');
 const app = express();
+const assetsPath = path.join(__dirname, 'public');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(assetsPath));
 
 const links = [
   { href: '/', text: 'Home' },
   { href: 'about', text: 'About' },
 ];
+
+const users = ['Rose', 'Cake', 'Biff'];
 app.get('/', (req, res) => {
-  res.render('index', { links });
+  res.render('index', { links, users });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { links });
 });
 
 app.listen(3000, () => {
