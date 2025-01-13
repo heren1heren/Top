c760c434bdfb8154cce1b07469c7bfc7244bdeab
heren <heren1heren@gmail.com>
2025-01-13 09:18:50 -0700

    ...

--- Node/Mini-Message-Board/app.js
+++ Node/Mini-Message-Board/app.js
@@ -5,8 +5,8 @@ const assetsPath = path.join(__dirname, 'public');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
-
 const messages = [
   {
     text: 'Hi there!',
@@ -25,7 +25,25 @@ app.get('/', (req, res) => {
 app.get('/new', (req, res) => {
   res.render('form', { title: 'message Form' });
 });
-
+//post
+app.post('/new', (req, res) => {
+  console.log(req.body);
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageAuthor,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
+app.get('/:messageText?', (req, res) => {
+  // compare messageText with messages array
+  console.log(req.query);
+  res.render('message-detail', {
+    messageText: req.params.messageText,
+    messageAuthor: req.query.author,
+    messageDate: req.query.added,
+  });
+});
 app.listen(3000, () => {
   console.log('    app is listening at port: localhost:3000');
 });
