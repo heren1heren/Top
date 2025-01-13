7dde030bf31b0a50b626991d1132cd228940884c
heren <heren1heren@gmail.com>
2025-01-13 07:30:18 -0700

    ...

--- Node/Views/app.js
+++ Node/Views/app.js
@@ -4,6 +4,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const links = [
+  { href: '/', text: 'Home' },
+  { href: 'about', text: 'About' },
+];
 app.get('/', (req, res) => {
-  res.render('index', { message: 'EJS rocks!' });
+  res.render('index', { links });
+});
+
+app.listen(3000, () => {
+  console.log('    app is listening at port: localhost:3000');
 });
