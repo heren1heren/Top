644dfdbe380280c837d9d0023bae9afaefc8fcfe
heren <heren1heren@gmail.com>
2025-01-13 07:13:14 -0700

    commit to pull

--- Node/ExpressLesson/app.js
+++ Node/ExpressLesson/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/about-me', (req, res) => res.send('Hello, world!')); // change this one with html file and we are done !!!
+
+app.get('/meIsChanging', (req, res) => res.send('Hello, world!'));
+
+app.get('/meJustRealizeSomethingThatMakeMeHappier', (req, res) =>
+  res.send('Hello, world!')
+);
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
--- Node/server.js
+++ Node/server.js
@@ -1,13 +1,5 @@
-const axios = require('axios');
-
-axios
-  .get(
-    'https://github.com/nodejs/nodejs.dev/blob/aa4239e87a5adc992fdb709c20aebb5f6da77f86/content/learn/node-js-web-server/node-make-http-requests.en.md'
-  )
-  .then((res) => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+let i = 0;
+setInterval(() => {
+  console.log(`interation ${i}`);
+  i++;
+}, 1000);
